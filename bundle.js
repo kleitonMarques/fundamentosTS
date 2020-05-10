e8afacd654e8a3f9e15accc18cff50141e0422c1
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-10 17:58:51 -0300

    Arrow Function

--- bundle.js
+++ bundle.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var arr = [1, 3, 4, 5, 6];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
+
+var teste = function teste() {
+  return {
+    nome: 'Kleiton'
+  };
+};
+
+console.log(teste());
