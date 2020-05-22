e566fab8733a38ea0d6d3820976102f8f4fbc6ad
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-22 16:23:55 -0300

    Template string

--- bundle.js
+++ bundle.js
@@ -1,6 +1,1 @@
 "use strict";
-
-var usuario = 'Diego';
-var idade = 23;
-console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
-console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos"));
