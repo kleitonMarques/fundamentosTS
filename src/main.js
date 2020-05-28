a12b5510832e501fc283d1ab1d27f048a3a68d10
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-28 14:30:47 -0300

    Loading e Error

--- src/main.js
+++ src/main.js
@@ -13,35 +13,57 @@ class App {
         this.formEl.onsubmit = event => this.addRepositoty(event)
     }
 
+    setLoading(loading = true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span')
+            loadingEl.appendChild(document.createTextNode('Carregando'))
+            loadingEl.setAttribute('id', 'loading')
+
+            this.formEl.appendChild(loadingEl)
+        } else {
+            document.getElementById('loading').remove()
+        }
+    }
+
     async addRepositoty(event) {
         event.preventDefault()
         const repoInput = this.inputEl.value
 
-        if(repoInput.length === 0)
-            return;
+        try {
+            if(repoInput.length === 0)
+                return;
 
-        const response = await api.get(`/users/${repoInput}`)
+            this.setLoading()
 
-        console.log(response)
+            const response = await api.get(`/users/${repoInput}`)
 
-        const { name, bio, html_url, avatar_url } = response.data
+            console.log(response)
 
-        this.repositories.push({
-            name,
-            bio,
-            avatar_url,
-            html_url,
-        })
+            const { name, bio, html_url, avatar_url } = response.data
 
-        this.inputEl.value = '';
+            this.repositories.push({
+                name,
+                bio,
+                avatar_url,
+                html_url,
+            })
 
-        this.render()
+            this.inputEl.value = '';
+
+            this.render()
+        } catch {
+            alert('O repositório não existe!')
+        }
+
+        this.setLoading(false)
     }
 
     render() {
         this.listEl.innerHTML = ''
 
         this.repositories.forEach(repo => {
+
+
             let imgEl = document.createElement('img')
             imgEl.setAttribute('src', repo.avatar_url)
 
@@ -49,7 +71,11 @@ class App {
             titleEl.appendChild(document.createTextNode(repo.name))
 
             let descriptionEl = document.createElement('p')
-            descriptionEl.appendChild(document.createTextNode(repo.bio))
+            if(repo.bio === null) {
+                descriptionEl.innerHTML = '</br>'
+            } else {
+                descriptionEl.appendChild(document.createTextNode(repo.bio))
+            }
 
             let linkEl = document.createElement('a')
             linkEl.setAttribute('target', '_blank')
